1bcbcd9080809ed4abaa5f47eecab9f2ee35da95
choridev <huicheol.yi@dsrvlabs.com>
2026-03-22 18:33:13 +0900

    Change timeoutCommit to 0

--- 0318/gaia/vendor/github.com/cometbft/cometbft/consensus/state.go
+++ 0318/gaia/vendor/github.com/cometbft/cometbft/consensus/state.go
@@ -1030,7 +1030,8 @@ func (cs *State) handleTxsAvailable() {
 		}
 
 		// +1ms to ensure RoundStepNewRound timeout always happens after RoundStepNewHeight
-		timeoutCommit := cs.StartTime.Sub(cmttime.Now()) + 1*time.Millisecond
+		// timeoutCommit := cs.StartTime.Sub(cmttime.Now()) + 1*time.Millisecond
+		timeoutCommit = 0
 		cs.scheduleTimeout(timeoutCommit, cs.Height, 0, cstypes.RoundStepNewRound)
 
 	case cstypes.RoundStepNewRound: // after timeoutCommit
